da21bf16d3a19deada3308f019ed6289e984152e
miskay <miskayapp@gmail.com>
2022-02-18 13:19:13 +0100

    v1.7

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import prof from '../img/PROF.PNG';
 
 const Hero=() => {
     return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 justify-items-center sm:py-16' >
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 justify-items-center sm:py-16 pt-16' >
             <div className='self-center'>
                 <h1 className='text-3xl font-Bungee'>
                     Hi,
@@ -15,9 +15,13 @@ const Hero=() => {
                 <p className='text-2xl font-Bungee2'>
                     Software Developer.
                 </p>
-                <p className='text-xs font-Bungee2'>
+                <p className='text-xs font-Bungee2 '>
                     Front-end Engineer | Swift | Flutter
                 </p>
+                <div className='py-6 W-24'>
+                    <button class=" rounded-full bg-stone-400 shadow-lg shadow-gray-500/50  text-sm font-Bungee2  px-3">Contact Me</button>
+                </div>
+
             </div>
 
 
