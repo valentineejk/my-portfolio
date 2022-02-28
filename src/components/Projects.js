043e38016c5a8fbb461055871102dd114ee83837
miskay <miskayapp@gmail.com>
2022-02-28 23:49:35 +0100

    py adjustments

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,7 @@ const callouts=[
 ]
 const Projects=() => {
     return (
-        <div>
+        <div >
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
                 <div className="max-w-2xl mx-auto    lg:max-w-none">
                     <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl text-center sm:pb-6 pb-6 py-16 font-Bungee2">Projects</h2>
--- src/components/Technicals.js
+++ src/components/Technicals.js
@@ -13,14 +13,14 @@ const Technicals=() => {
     return (
         <div >
 
-            <div className='sm:py-8 py-8'>
-                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl text-center font-Bungee2 sm:py-8 py-16">Tech Stack</h2>
 
-            </div>
 
             <div className="max-w-2xl mx-auto px-4 py-16 sm:px-6  lg:max-w-7xl">
+                <div>
+                    <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl text-center font-Bungee2 sm:py-8">Tech Stack</h2>
 
-                <div className=' grid sm:grid-cols-5 grid-cols-3 gap-4 justify-items-center  h-16 sm:py-8  py-8' >
+                </div>
+                <div className=' grid sm:grid-cols-5 grid-cols-3 gap-4 justify-items-center  h-16 sm:py-8  py-16' >
                     <Swift className=" sm:w-28 sm:h-28  w-16 h-16 sm:rounded-full rounded-full shadow-inner  " />
                     {/* <Flutter className="sm:w-28 sm:h-28 w-16 h-16  rounded-full shadow-inner " /> */}
                     <Reactj className="sm:w-28 sm:h-28 w-16 h-16  rounded-full shadow-inner " />
